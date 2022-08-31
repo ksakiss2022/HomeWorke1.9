2b5e4c297a5e1177ae857b70d71091d9f2a259e3
Swetlana <sweta_zaika@mail.ru>
2022-09-01 00:32:35 +0300

    Проверьте пожалуйста дз

--- src/Author.java
+++ src/Author.java
@@ -1,5 +1,21 @@
 public class Author {
+    private String firstName;
+    private String lastName;
 
+    public Author(String firstName, String lastName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-    }
+        Author firstAutor = new Author("Artur", "Konan Doil");
 
+
+        System.out.println("firstAutor.firstName = " + firstAutor.getFirstName());
+        System.out.println("firstAutor.lastName = " + firstAutor.getLastName());
+
+
+        Author seconAuthor = new Author("Barbara", "Mitchelhill");
+
+
+        System.out.println("seconAuthor.firstName = " + seconAuthor.getFirstName());
+        System.out.println("seconAuthor.lastName = " + seconAuthor.getLastName());
+
+        Author alicesAdventuresInWonderland = new Author("Lewis", "Carroll");
+
+
+        Book myBookFirst = new Book("theLostWorld", 1984);
+
+
+        System.out.println("myBookFirst.titleOfTheBook = " + myBookFirst.titleOfTheBook());
+        System.out.println("myBookFirst.yearOfPublicationOfTheBook = " + myBookFirst.getyearOfPublicationOfTheBook());
+
+        Book myBookSecond = new Book("seeYouSoon", 2000);
+
+
+        System.out.println("myBookSecond.titleOfTheBook = " + myBookSecond.titleOfTheBook());
+        System.out.println("myBookSecond.yearOfPublicationOfTheBook = " + myBookSecond.getyearOfPublicationOfTheBook());
+
+        myBookSecond.setYearOfPublicationOfTheBook(2022);
+        System.out.println("myBookSecond.yearOfPublicationOfTheBook = " + myBookSecond.getyearOfPublicationOfTheBook());
+    }
 }
