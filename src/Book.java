0a60649ec4cdc43427d78a4f20655dbcf9f7f800
Swetlana <sweta_zaika@mail.ru>
2022-09-01 00:35:50 +0300

    Проверьте пожалуйста дз

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,24 @@
 public class Book {
 
+    private String titleOfTheBook;
+    private int yearOfPublicationOfTheBook;
+
+    public Book(String titleOfTheBook, int yearOfPublicationOfTheBook) {
+        this.yearOfPublicationOfTheBook = yearOfPublicationOfTheBook;
+        this.titleOfTheBook = titleOfTheBook;
+    }
+
+    public String titleOfTheBook() {
+        return this.titleOfTheBook;
+
+    }
+
+    public int getyearOfPublicationOfTheBook() {
+        return this.yearOfPublicationOfTheBook;
+
+    }
+
+    public void setYearOfPublicationOfTheBook(int yearOfPublicationOfTheBook) {
+        this.yearOfPublicationOfTheBook = yearOfPublicationOfTheBook;
+    }
 }
